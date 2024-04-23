b321de29bd20eca8ba0f4ad73cf00b57d797b452
omkarssinha <80606973+omkarssinha@users.noreply.github.com>
2024-04-23 15:30:49 -0400

    Committing code

---
+++ src/main/java/com/example/mailsend/MailSendApplication.java
@@ -0,0 +1,14 @@
+package com.example.mailsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MailSendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSendApplication.class, args);
+	}
+
+}
